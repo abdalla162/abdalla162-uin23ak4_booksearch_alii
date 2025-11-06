87256ec0ce81ceb3acec2c6f7a25d3a192c0d59a
abdalla162 <abdalla162@hotmail.com>
2025-11-06 09:34:47 +0100

    Update vite.config.js

--- Oblig4/vite.config.js
+++ Oblig4/vite.config.js
@@ -1,7 +1,8 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // ← viktig. hvis du har en annen base, endre til denne.
 })
